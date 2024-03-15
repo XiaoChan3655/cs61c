fbb4f69743390fa20822130634e0eae0e9b0af3a
xiaofeng <2404774642@qq.com>
2024-03-15 16:40:01 +0800

    lab01

--- fa23-lab/lab01/ex10_ll_cycle.c
+++ fa23-lab/lab01/ex10_ll_cycle.c
@@ -3,4 +3,20 @@
 
 int ll_has_cycle(node *head) {
     /* TODO: Implement ll_has_cycle */
+    if (head == NULL) {
+        return 0;
+    };
+    int ll_help(node* fast_ptr, node* slow_ptr){
+        if ((fast_ptr->next == NULL) || (fast_ptr->next->next == NULL)) {  
+            return 0;  
+        }
+        fast_ptr = fast_ptr->next->next; 
+        slow_ptr = slow_ptr->next;
+        if ((fast_ptr->value == slow_ptr->value) && (fast_ptr->next == slow_ptr->next)) {
+            return 1;
+        }else {
+            return ll_help(fast_ptr, slow_ptr);
+        };
+    }
+    ll_help(head, head);
 }
